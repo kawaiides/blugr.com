1b7c2ddc8de0f89ce6edb2275da2e834378e02aa
Shyam Sunder <shyam@Shyams-MacBook-Air.local>
2025-03-10 14:12:45 +0530

    sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,8 +1,10 @@
-import { blogs } from "@/lib/data"
+import { connectToDatabase } from "@/lib/mongodb"
 
 export default async function sitemap() {
   const baseUrl = "https://blogplatform.com"
-
+  const client = await connectToDatabase()
+  const collection = client.db("blugr").collection("generated-texts")
+  const blogs = await collection.find().toArray()
   // Get all blog posts
   const blogUrls = blogs.map((blog) => ({
     url: `${baseUrl}/blog/${blog.id}`,
